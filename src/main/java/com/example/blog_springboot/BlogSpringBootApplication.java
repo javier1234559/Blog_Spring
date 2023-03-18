61bba1fea3d206312eaa63deee5cd5ed281f99f7
javier1234559 <nhat1234559@gmail.com>
2023-03-19 01:53:10 +0700

    first-commit2

---
+++ src/main/java/com/example/blog_springboot/BlogSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSpringBootApplication.class, args);
+    }
+
+}
