9d0e679ec60631d2da0acd964960d228e3b0d9fc
javier1234559 <nhat1234559@gmail.com>
2023-05-02 01:04:28 +0700

    complete authenticated with encodeded

--- src/main/java/com/example/blog_springboot/security/SpringSecurity.java
+++ src/main/java/com/example/blog_springboot/security/SpringSecurity.java
@@ -3,16 +3,13 @@ package com.example.blog_springboot.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -31,35 +28,44 @@ public class SpringSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests (auth -> {
-                    auth.requestMatchers("/admin/**").hasRole("ADMIN");
-                    auth.requestMatchers("/","/about","/contact","/posts/**", "/register", "/error", "/admin/**","/uploaded/**").permitAll();
-                    auth.requestMatchers("/templates/**","/static/**","/product/**","/dashboard/**").permitAll();
-                    auth.requestMatchers("/api/posts/**","/api/posts").permitAll();
-                    auth.anyRequest().authenticated();
+                            auth.requestMatchers("/admin/**").hasRole("ADMIN");
+                            auth.requestMatchers("/","/about","/contact","/posts/**", "/register", "/error", "/admin/**","/uploaded/**").permitAll();
+                            auth.requestMatchers("/templates/**","/static/**","/product/**","/dashboard/**").permitAll();
+                            auth.requestMatchers("/api/posts/**","/api/posts").permitAll();
+                            auth.anyRequest().authenticated();
                         })
-//                .formLogin(form -> {
-//                            form.usernameParameter("email");
-//                            form.passwordParameter("pass");
-//                            form.loginPage("/login");
-//                            form.loginProcessingUrl("/login");
-//                            form.defaultSuccessUrl("/");
-//                            form.permitAll();
-//                        })
-                .formLogin();
+                .formLogin(form -> {
+                            form.usernameParameter("email");
+                            form.passwordParameter("pass");
+                            form.loginPage("/login");
+                            form.loginProcessingUrl("/login");
+                            form.defaultSuccessUrl("/");
+                            form.permitAll();
+                        });
 
         return http.build();
     }
+
+
     @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-        UserDetails admin = User.withUsername("hach")
-                .password(encoder.encode("hacheery"))
-                .roles("ADMIN")
-                .build();
-        UserDetails user = User.withUsername("nhat")
-                .password(encoder.encode("123"))
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(admin, user);
+    public AuthenticationManager authenticationManager(PasswordEncoder encoder) {
+        var authProvider = new DaoAuthenticationProvider();
+        authProvider.setPasswordEncoder(encoder);
+        authProvider.setUserDetailsService(userDetailsService);
+        return new ProviderManager(authProvider);
     }
+
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//        UserDetails admin = User.withUsername("hach")
+//                .password(encoder.encode("hacheery"))
+//                .roles("ADMIN")
+//                .build();
+//        UserDetails user = User.withUsername("nhat1234559@gmail.com")
+//                .password(encoder.encode("123"))
+//                .roles("USER")
+//                .build();
+//        return new InMemoryUserDetailsManager(admin, user);
+//    }
 
 }
