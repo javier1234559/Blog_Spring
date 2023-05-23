38f8a008b8613947b64644ce633c718dae8ca8da
javier1234559 <nhat1234559@gmail.com>
2023-05-23 19:12:14 +0700

    forgot to change SERVER URL in js file

--- src/main/resources/static/product/js/custom.js
+++ src/main/resources/static/product/js/custom.js
@@ -1,8 +1,8 @@
 //Function Post AJAX
 //--------------- Global Data -------------
 let posts = null;
-const SERVER_URL = 'http://localhost:8080';
-// const SERVER_URL = 'https://blogspring-production.up.railway.app';
+// const SERVER_URL = 'http://localhost:8080';
+const SERVER_URL = 'https://blogspring-production.up.railway.app';
 
 //------------- COMMON CLASS ------------
 class ApiFacade {
